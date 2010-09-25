cecb6fe59656f03a6724b31f8289d6efa90ccab2
koseki <koseki@gmail.com>
2010-09-25 21:46:33 +0900

    do nothing if focus to input field or text area.

--- chrome/src/copyfixer.js
+++ chrome/src/copyfixer.js
@@ -6,20 +6,23 @@ function copyfixer(event) {
     var isMac = (navigator.platform.indexOf("Mac") != -1);
     if ((! isMac && ! event.ctrlKey) || (isMac && ! event.metaKey)) return;
     if (isSelected()) return;
+alert("exec");
 
     var crlf  = isWin ? "\r\n" : "\n";
     var txt   = document.title + crlf + document.location.href + crlf + crlf;
-
     chrome.extension.sendRequest({command: "copyfixerCopy", data: txt });
 }
 
 function isSelected() {
     var sel = window.getSelection();
-    if (sel.rangeCount == 0) return false;
+    if (sel.rangeCount <= 0) return false;
     if (sel.rangeCount > 1)  return true;
 
     var range = sel.getRangeAt(0);
+    if (! range.collapsed) return true;
     if (range.startContainer != range.endContainer) return true;
     if (range.startOffset    != range.endOffset)    return true;
+    if (document.activeElement.tagName.toLowerCase() != "body") return true;
+
     return false;
 }
